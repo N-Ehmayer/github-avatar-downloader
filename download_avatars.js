bfef45aa34819f81c61c16da0e7a44a05b9e46e5
Nic <nic.ehmayer92@gmail.com>
2018-01-22 22:15:50 +0000

    Added authorization token forGithub API + added User-Agent to header.

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,23 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + "contributors/",
+    headers: {
+      'User-Agent': 'request'
 
-};
+      // Authorization Token here???
+    }
+  };
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log('Errors: ', err);
-  console.log('Result: ', result);
-});
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log('Errors: ', err);
+//   console.log('Result: ', result);
+// });
