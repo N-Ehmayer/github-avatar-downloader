2dc14985a382b349b88bad48eafd5f84220cddea
Nic <nic.ehmayer92@gmail.com>
2018-01-23 05:20:21 +0000

    Initialized 'downloadImageByURL function with hard coded arguments.

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,42 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + "contributors/",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request'
-
-      // Authorization Token here???
     }
   };
 
+
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    var parsedBody = JSON.parse(body);
+    cb(err, parsedBody);
+
+    parsedBody.forEach(function(user) {
+      console.log(user.avatar_url);
+    });
   });
 }
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log('Errors: ', err);
-//   console.log('Result: ', result);
-// });
+
+//getRepoContributors('jquery', 'jquery', function(err, result) {});
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(error) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log("Response Status Code: ", response.statusCode);
+      console.log('Download complete.');
+    })
+    .pipe(fs.createWriteStream(filePath));
+    console.log("Downloading images...");
+}
+
+downloadImageByURL("https://avatars0.githubusercontent.com/u/1615?v=4", 'avatars/avatar.png');
